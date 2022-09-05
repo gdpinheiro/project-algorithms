218232b6d2726c0802cfee2c158bd2af88f82f65
black <gikigiio@gmail.com>
2022-09-05 13:27:00 -0300

    Req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    counter = 0
+    if target_time is None:
+        return None
+    for i in permanence_period:
+        if type(i[0]) != int or type(i[1]) != int:
+            return None
+        if i[0] <= target_time <= i[1]:
+            counter += 1
+    return counter

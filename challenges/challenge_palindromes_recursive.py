a2a2bc3ad5f21bf635788298af58a556e645635a
black <gikigiio@gmail.com>
2022-09-05 13:54:44 -0300

    Req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,12 +1,16 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    split_word = []
-
     if word == "":
         return False
 
-    for letter in word:
-        split_word.append(letter)
+    if len(word) == 1:
+        return True
 
-    split_word.reverse()
+    if len(word) == 2 and word[0] == word[1]:
+        return True
 
-    return word == "".join(split_word)
+    if word[low_index] == word[high_index]:
+        sliced_word = word[1:-1]
+        new_higher_index = len(sliced_word) - 1
+        return is_palindrome_recursive(sliced_word, 0, new_higher_index)
+    else:
+        return False

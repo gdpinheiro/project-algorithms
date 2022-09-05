119b7bccb2b85ad86de4d4630097544887a76369
black <gikigiio@gmail.com>
2022-09-05 14:23:03 -0300

    Req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,21 @@
+def sort_strings(word):
+    word_len = len(word)
+
+    for index in range(word_len):
+        for item in range(word_len - index - 1):
+            if word[item] > word[item + 1]:
+                current_element = word[item]
+                word[item] = word[item + 1]
+                word[item + 1] = current_element
+
+    return word
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    lower_first_string = list(first_string.lower())
+    lower_second_string = list(second_string.lower())
+
+    sorted_first_string = sort_strings(lower_first_string)
+    sorted_second_string = sort_strings(lower_second_string)
+
+    return sorted_first_string == sorted_second_string
